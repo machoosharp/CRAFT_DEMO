2183542a2a9a47b0add364b01b83da8edb352440
machoosharp <sharpart555@yahoo.com>
2024-02-28 00:14:49 -0500

    Adds camera movement for mouse
    Adds simple camera movement for mouse to character model, can look up and down while moving, mouse is hidden from view while in game

--- character_controller.gd
+++ character_controller.gd
@@ -1,12 +1,16 @@
 extends CharacterBody3D
 
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+const SPEED = 10.0
+const JUMP_VELOCITY = 13
+
+@export var sensitivity = 200
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity") * 5
 
+func _ready():
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -21,11 +25,23 @@ func _physics_process(delta):
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("left", "right", "forward", "backward")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
+
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
+	if position.y < -100:
+		position = Vector3(0, 0, 0)
+
 	move_and_slide()
+
+func _input(event):
+	
+	if event is InputEventMouseMotion and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+		rotation.y -= event.relative.x / sensitivity
+		$Camera3D.rotation.x -= event.relative.y / sensitivity
+		$Camera3D.rotation.x = clamp($Camera3D.rotation.x, deg_to_rad(-85), deg_to_rad(85))
