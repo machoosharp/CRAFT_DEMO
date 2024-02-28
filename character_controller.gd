644ca80eb8406faeedaec2385149093eaead8073
machoosharp <sharpart555@yahoo.com>
2024-02-28 00:16:18 -0500

    Adds ability to press ESC to regain mouse
    Found it difficult to get in and out of game when mouse was captured by display, made it so mouse can be relinquished and regained with the press of ESC

--- character_controller.gd
+++ character_controller.gd
@@ -4,6 +4,8 @@ extends CharacterBody3D
 const SPEED = 10.0
 const JUMP_VELOCITY = 13
 
+var locked = false
+
 @export var sensitivity = 200
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
@@ -13,23 +15,28 @@ func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 func _physics_process(delta):
+
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("up") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+		if Input.is_action_just_pressed("up") and is_on_floor():
+			velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("left", "right", "forward", "backward")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+		# Get the input direction and handle the movement/deceleration.
+		# As good practice, you should replace UI actions with custom gameplay actions.
+		var input_dir = Input.get_vector("left", "right", "forward", "backward")
+		var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		if direction:
+			velocity.x = direction.x * SPEED
+			velocity.z = direction.z * SPEED
 
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+			velocity.z = move_toward(velocity.z, 0, SPEED)
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
@@ -45,3 +52,17 @@ func _input(event):
 		rotation.y -= event.relative.x / sensitivity
 		$Camera3D.rotation.x -= event.relative.y / sensitivity
 		$Camera3D.rotation.x = clamp($Camera3D.rotation.x, deg_to_rad(-85), deg_to_rad(85))
+	
+	elif event is InputEventKey:
+		if event.keycode == KEY_ESCAPE:
+			if event.pressed and not locked:
+				locked = true
+				if Input.mouse_mode != Input.MOUSE_MODE_VISIBLE:
+					Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+
+				elif Input.mouse_mode == Input.MOUSE_MODE_VISIBLE:
+					Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+			elif not event.pressed:
+				locked = false
+
