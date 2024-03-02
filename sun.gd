d9a9e609bd7c7a26987a15fe35aa2fb5b606d930
machoosharp <sharpart555@yahoo.com>
2024-03-02 16:04:00 -0500

    Add Day night cycle
    Adds simple day night cycle, minus the night part :P

---
+++ sun.gd
@@ -0,0 +1,19 @@
+extends DirectionalLight3D
+
+@export var x: float
+@export var y: float
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+
+	var day_speed = 200.0
+	
+	var ticks = Time.get_ticks_msec() * 0.1
+	
+	print(ticks)
+
+	var t = ( int( ticks ) ) / day_speed
+
+	rotation.x = deg_to_rad(t-180)
+
+
