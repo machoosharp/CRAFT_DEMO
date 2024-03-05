e10b305f2b37839019a97d74183d7a692dc73ec2
machoosharp <sharpart555@yahoo.com>
2024-03-05 13:56:05 -0500

    adds Main Menu
    Adds main menu scene that can select all the other scenes, makes player menu exit bar go back to main menu instead of exiting game.

---
+++ main_menu.gd
@@ -0,0 +1,18 @@
+extends Control
+
+@onready var character = $"../.."
+
+func _on_chop_log_pressed():
+	print('log chop')
+	get_tree().change_scene_to_file("res://main.tscn")
+
+func _on_mesh_proc_pressed():
+	print('mesh world')
+	get_tree().change_scene_to_file("res://mesh_world/procedural_world.tscn")
+
+func _on_block_proc_pressed():
+	print('block world')
+	get_tree().change_scene_to_file("res://minecraft_clone/minecraft_clone.tscn")
+
+func _on_exit_pressed():
+	get_tree().quit()
